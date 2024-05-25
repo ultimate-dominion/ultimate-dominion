7f015525c2191212d8a67d8bfa7dddc035db7434
ECWireless <elliott@coopallc.com>
2024-05-25 08:13:43 -0600

    Try pushing out folder

---
+++ packages/contracts/out/IIncrementSystem.sol/IIncrementSystem.abi.json.d.ts
@@ -0,0 +1,15 @@
+declare const abi: [
+  {
+    "type": "function",
+    "name": "app__increment",
+    "inputs": [],
+    "outputs": [
+      {
+        "name": "",
+        "type": "uint32",
+        "internalType": "uint32"
+      }
+    ],
+    "stateMutability": "nonpayable"
+  }
+]; export default abi;
