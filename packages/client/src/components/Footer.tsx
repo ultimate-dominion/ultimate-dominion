fcc9dc14ff4e2ac72dd564e754b2d745aa6cd8d9
Fluffy9 <Fluffy9@users.noreply.github.com>
2024-05-31 21:31:56 +0000

    Fixed

--- packages/client/src/components/Footer.tsx
+++ packages/client/src/components/Footer.tsx
@@ -1,83 +1,24 @@
-import {
-  Box,
-  Flex,
-  Heading,
-  Image,
-  Link,
-  List,
-  ListItem,
-  VStack,
-} from '@chakra-ui/react';
+import { Box, Link, Stack } from '@chakra-ui/react';
 
 export const Footer = (): JSX.Element => {
   return (
-    <Box padding={'10px'} margin-top={'50px'}>
-      <VStack alignItems="start" h="100%" p={4} w="100%">
-        <Flex height="100%" width="100%">
-          <Box width="25%" padding={'5px'}>
-            <Link href="https://raidguild.org">
-              <Image src="/images/raidguild.webp"></Image>
-            </Link>
-          </Box>
-          <Box width="25%" padding={'5px'}>
-            <List>
-              <ListItem>
-                <Heading as={'h4'}>Documentation</Heading>
-              </ListItem>
-              <ListItem>
-                <Link href="https://raidguild.org">Link</Link>
-              </ListItem>
-              <ListItem>
-                <Link href="https://raidguild.org">Link</Link>
-              </ListItem>
-              <ListItem>
-                <Link href="https://raidguild.org">Link</Link>
-              </ListItem>
-              <ListItem>
-                <Link href="https://raidguild.org">Link</Link>
-              </ListItem>
-            </List>
-          </Box>
-          <Box width="25%" padding={'5px'}>
-            <List>
-              <ListItem>
-                <Heading as={'h4'}>Policy</Heading>
-              </ListItem>
-              <ListItem>
-                <Link href="https://raidguild.org">Link</Link>
-              </ListItem>
-              <ListItem>
-                <Link href="https://raidguild.org">Link</Link>
-              </ListItem>
-              <ListItem>
-                <Link href="https://raidguild.org">Link</Link>
-              </ListItem>
-              <ListItem>
-                <Link href="https://raidguild.org">Link</Link>
-              </ListItem>
-            </List>
-          </Box>
-          <Box flex="1" padding={'5px'}>
-            <List>
-              <ListItem>
-                <Heading as={'h4'}>Socials</Heading>
-              </ListItem>
-              <ListItem>
-                <Link href="https://raidguild.org">Link</Link>
-              </ListItem>
-              <ListItem>
-                <Link href="https://raidguild.org">Link</Link>
-              </ListItem>
-              <ListItem>
-                <Link href="https://raidguild.org">Link</Link>
-              </ListItem>
-              <ListItem>
-                <Link href="https://raidguild.org">Link</Link>
-              </ListItem>
-            </List>
-          </Box>
-        </Flex>
-      </VStack>
+    <Box fontSize="smaller" margin-top="50px" padding="20px">
+      <Stack direction={['column', 'row']} float="left">
+        <Stack direction="row">
+          <Link>About</Link>
+          <Link>Map Info</Link>
+        </Stack>
+        <Stack direction="row">
+          <Link>Create Map</Link>
+          <Link>Guild Info</Link>
+        </Stack>
+      </Stack>
+
+      <Stack direction="row" float="right">
+        <Link>Discord</Link>
+        <Link>Twitter</Link>
+        <Link>T&C</Link>
+      </Stack>
     </Box>
   );
 };
--- packages/client/src/components/Header.tsx
+++ packages/client/src/components/Header.tsx
@@ -1,11 +1,10 @@
 import {
   Box,
   Button,
+  Center,
+  Divider,
   Heading,
-  Menu,
-  MenuButton,
-  MenuItem,
-  MenuList,
+  Stack,
   useDisclosure,
 } from '@chakra-ui/react';
 
@@ -15,21 +14,19 @@ export const Header = (): JSX.Element => {
   const { isOpen, onOpen, onClose } = useDisclosure();
 
   return (
-    <Box padding={'10px'}>
-      <Menu placement="top-end">
-        <MenuButton as={Box} textAlign={'right'} backgroundColor={'lightgray'}>
-          <Heading as={'h1'} margin={'10px'}>
-            Ultimate Dominion
-          </Heading>
-        </MenuButton>
-        <MenuList>
-          <MenuItem>
-            <Button margin={'0px auto'} onClick={onOpen}>
-              Connect
-            </Button>
-          </MenuItem>
-        </MenuList>
-      </Menu>
+    <Box padding="10px">
+      <Stack backgroundColor="lightgray" direction="row" padding="10px">
+        <Heading as="h1" margin="10px" w="50%">
+          Ultimate Dominion
+        </Heading>
+        <Divider opacity="0"></Divider>
+        <Center>
+          <Button margin="0px auto" onClick={onOpen} textAlign="right">
+            Connect
+          </Button>
+        </Center>
+      </Stack>
+
       <ConnectWalletModal isOpen={isOpen} onClose={onClose} />
     </Box>
   );
