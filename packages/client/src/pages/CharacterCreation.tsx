44e625980d2c75b3635ce4df47fa12708069407d
ECWireless <elliott@coopallc.com>
2024-06-03 09:13:05 -0600

    Remove Container from CharacterCreation

--- packages/client/src/pages/CharacterCreation.tsx
+++ packages/client/src/pages/CharacterCreation.tsx
@@ -6,7 +6,6 @@ import {
   Card,
   CardBody,
   Center,
-  Container,
   Flex,
   Heading,
   Input,
@@ -19,77 +18,81 @@ import {
 
 export const CharacterCreation = (): JSX.Element => {
   return (
-    <Box>
-      <Stack direction={['column', 'row']} justifyContent="center">
-        <Container border="2px solid" margin="10px" padding="20px" width="50%">
-          <VStack>
-            <Flex height="100%" width="100%">
-              <Center margin="10px auto" width="40%">
-                <Avatar size="lg"></Avatar>
-              </Center>
-              <Box alignContent="left" margin="10px auto" width="60%">
-                <Stack spacing={3}>
-                  <Input placeholder="Outline" variant="outline" />
-                  <Input placeholder="Outline" variant="outline" />
-                </Stack>
-              </Box>
-            </Flex>
-          </VStack>
-          <VStack>
-            <Box width="100%">
-              <Textarea placeholder="Here is a sample placeholder" />
+    <Stack
+      direction={['column', 'row']}
+      gap={6}
+      justifyContent="center"
+      my={20}
+      px={12}
+    >
+      <Box border="2px solid" padding="20px" width="50%">
+        <VStack>
+          <Flex height="100%" width="100%">
+            <Center margin="10px auto" width="40%">
+              <Avatar size="lg"></Avatar>
+            </Center>
+            <Box alignContent="left" margin="10px auto" width="60%">
+              <Stack spacing={3}>
+                <Input placeholder="Outline" variant="outline" />
+                <Input placeholder="Outline" variant="outline" />
+              </Stack>
             </Box>
-          </VStack>
-        </Container>
-        <Container border="2px solid" margin="10px" padding="20px" width="50%">
-          <VStack textAlign="left">
-            <Heading textAlign="left" width="100%">
-              Choose your Character
-            </Heading>
-            <ButtonGroup spacing="6" variant="outline">
-              <Button>Warrior</Button>
-              <Button>Mage</Button>
-              <Button>Rogue</Button>
-            </ButtonGroup>
-          </VStack>
-          <Stack direction="row" w="full">
-            <Heading textAlign="left">Stats</Heading>
-            <Button size="sm" variant="outline">
-              Roll Stats
-            </Button>
-            <Text>$Gold</Text>
-            <Text>00,00</Text>
-          </Stack>
-          <Stack direction="row">
-            <Stack direction="column">
-              <Stack direction="row">
-                <Text width="50%">HP - Hit</Text>
-                <Text align="right">1</Text>
-              </Stack>
-              <Stack direction="row">
-                <Text width="50%">STR - Strength</Text>
-                <Text align="right">2</Text>
-              </Stack>
-              <Stack direction="row">
-                <Text width="50%">AGI - Agility</Text>
-                <Text align="right">3</Text>
-              </Stack>
-              <Stack direction="row">
-                <Text width="50%">INT - Intelligence</Text>
-                <Text align="right">4</Text>
-              </Stack>
+          </Flex>
+        </VStack>
+        <VStack>
+          <Box width="100%">
+            <Textarea placeholder="Here is a sample placeholder" />
+          </Box>
+        </VStack>
+      </Box>
+      <Box border="2px solid" padding="20px" width="50%">
+        <VStack textAlign="left">
+          <Heading textAlign="left" width="100%">
+            Choose your Character
+          </Heading>
+          <ButtonGroup spacing="6" variant="outline">
+            <Button>Warrior</Button>
+            <Button>Mage</Button>
+            <Button>Rogue</Button>
+          </ButtonGroup>
+        </VStack>
+        <Stack direction="row" w="full">
+          <Heading textAlign="left">Stats</Heading>
+          <Button size="sm" variant="outline">
+            Roll Stats
+          </Button>
+          <Text>$Gold</Text>
+          <Text>00,00</Text>
+        </Stack>
+        <Stack direction="row">
+          <Stack direction="column">
+            <Stack direction="row">
+              <Text width="50%">HP - Hit</Text>
+              <Text align="right">1</Text>
             </Stack>
-            <Stack direction="column" w="50%">
-              <Text>Items - 1</Text>
-              <Card>
-                <CardBody>A Card</CardBody>
-              </Card>
-              <Link>Auction House ▶</Link>
+            <Stack direction="row">
+              <Text width="50%">STR - Strength</Text>
+              <Text align="right">2</Text>
+            </Stack>
+            <Stack direction="row">
+              <Text width="50%">AGI - Agility</Text>
+              <Text align="right">3</Text>
+            </Stack>
+            <Stack direction="row">
+              <Text width="50%">INT - Intelligence</Text>
+              <Text align="right">4</Text>
             </Stack>
           </Stack>
-          <Button width="100%">Wake Up</Button>
-        </Container>
-      </Stack>
-    </Box>
+          <Stack direction="column" w="50%">
+            <Text>Items - 1</Text>
+            <Card>
+              <CardBody>A Card</CardBody>
+            </Card>
+            <Link>Auction House ▶</Link>
+          </Stack>
+        </Stack>
+        <Button width="100%">Wake Up</Button>
+      </Box>
+    </Stack>
   );
 };
