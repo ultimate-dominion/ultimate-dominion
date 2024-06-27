78b350d3f951af15f683e40226a76d0fa707e81b
ECWireless <elliott@coopallc.com>
2024-06-27 09:13:23 -0400

    Display indexer sync status

--- packages/client/src/Routes.tsx
+++ packages/client/src/Routes.tsx
@@ -1,11 +1,33 @@
-import { Route, Routes } from 'react-router-dom';
+import { Text, VStack } from '@chakra-ui/react';
+import { useComponentValue } from '@latticexyz/react';
+import { SyncStep } from '@latticexyz/store-sync';
+import { singletonEntity } from '@latticexyz/store-sync/recs';
+import { Route, Routes, useLocation } from 'react-router-dom';
 
+import { useMUD } from './contexts/MUDContext';
 import { Character } from './pages/Character';
 import { CharacterCreation } from './pages/CharacterCreation';
 import { GameBoard } from './pages/GameBoard';
 import { Welcome } from './pages/Welcome';
 
 const AppRoutes: React.FC = () => {
+  const { pathname } = useLocation();
+  const {
+    components: { SyncProgress },
+  } = useMUD();
+
+  const syncProgress = useComponentValue(SyncProgress, singletonEntity);
+
+  if (syncProgress && syncProgress.step !== SyncStep.LIVE && pathname !== '/') {
+    return (
+      <VStack justify="center" h="100%">
+        <Text>
+          {syncProgress.message} {Math.round(syncProgress.percentage)}%
+        </Text>
+      </VStack>
+    );
+  }
+
   return (
     <Routes>
       <Route path="/" element={<Welcome />} />
