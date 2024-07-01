375bc6966fbe38693eb2e2a09fd627eac376dc3d
ECWireless <elliott@coopallc.com>
2024-07-01 09:43:46 -0600

    Add CopyText component

---
+++ packages/client/src/components/CopyText.tsx
@@ -0,0 +1,30 @@
+import { Text, Tooltip } from '@chakra-ui/react';
+import { useCallback, useState } from 'react';
+
+export const CopyText = ({
+  children,
+  text,
+}: {
+  children: JSX.Element;
+  text: string;
+}): JSX.Element => {
+  const [isCopied, setIsCopied] = useState(false);
+
+  const onCopy = useCallback(() => {
+    navigator.clipboard.writeText(text);
+    setIsCopied(prev => !prev);
+  }, [text]);
+
+  return (
+    <Tooltip
+      closeDelay={1000}
+      hasArrow
+      label={isCopied ? 'Copied!' : text}
+      placement="top"
+    >
+      <Text cursor="pointer" onClick={onCopy}>
+        {children}
+      </Text>
+    </Tooltip>
+  );
+};
