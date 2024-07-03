2c1746c736b48c25d62672eab7d478e074947acb
MrDeadCe11 <mister.deadce11@gmail.com>
2024-07-02 19:43:38 -0500

    added missing files

--- packages/contracts/test/CombatSystem.t.sol
+++ packages/contracts/test/CombatSystem.t.sol
@@ -1,8 +1,8 @@
 pragma solidity >=0.8.24;
 
 import {SetUp} from "./SetUp.sol";
-import {Classes, ItemType, EncounterType} from "@codegen/common.sol";
-import {StatsData, Stats} from "@tables/Stats.sol";
+import {Classes, ItemType} from "@codegen/common.sol";
+import {StatsData} from "@tables/Stats.sol";
 import "forge-std/console2.sol";
 import {PuppetModule} from "@latticexyz/world-modules/src/modules/puppet/PuppetModule.sol";
 import {UltimateDominionConfig} from "@codegen/index.sol";
@@ -13,10 +13,10 @@ import {IERC1155MetadataURI} from "@erc1155/IERC1155MetadataURI.sol";
 import {IERC1155} from "@erc1155/IERC1155.sol";
 import {registerERC1155} from "@erc1155/registerERC1155.sol";
 import {_erc1155SystemId} from "@erc1155/utils.sol";
-import {WeaponStats, ArmorStats, Action} from "@interfaces/Structs.sol";
+import {WeaponStats, ArmorStats} from "@interfaces/Structs.sol";
 import {ResourceIdLib} from "@latticexyz/store/src/ResourceId.sol";
 import {ResourceId, WorldResourceIdLib, WorldResourceIdInstance} from "@latticexyz/world/src/WorldResourceId.sol";
-import {_itemsSystemId, _mobSystemId} from "../src/utils.sol";
+import {_itemsSystemId} from "../src/utils.sol";
 import {
     GOLD_NAMESPACE,
     CHARACTERS_NAMESPACE,
@@ -28,81 +28,5 @@ import {
 import {GasReporter} from "@latticexyz/gas-report/src/GasReporter.sol";
 
 contract Test_CombatSystem is SetUp, GasReporter {
-    bytes32[] public defenders;
-    bytes32[] public attackers;
-    bytes32 entityId;
-
-    function setUp() public override {
-        super.setUp();
-        vm.prank(deployer);
-        world.grantAccess(_mobSystemId("UD"), address(this));
-
-        entityId = world.UD__spawnMob(1, 0, 0);
-
-        defenders.push(entityId);
-        attackers.push(bobCharacterId);
-    }
-
-    function test_createMatch() public {
-        vm.prank(bob);
-        bytes32 matchId = world.UD__createMatch(EncounterType.PvE, attackers, defenders);
-        assertEq(world.UD__getEncounter(matchId).start, block.timestamp);
-    }
-
-    function test_createMatch_Revert_Entities_Wrong_Position() public {
-        bytes32 entityId2 = world.UD__spawnMob(1, 0, 1);
-        defenders[0] = entityId2;
-        vm.prank(bob);
-        vm.expectRevert("COMBAT SYSTEM: INVALID PVE");
-        bytes32 matchId = world.UD__createMatch(EncounterType.PvE, attackers, defenders);
-    }
-
-    function test_CreateMatch_Revert_ENTITY_OCCUPIED() public {
-        vm.prank(bob);
-        bytes32 matchId = world.UD__createMatch(EncounterType.PvE, attackers, defenders);
-        assertEq(world.UD__getEncounter(matchId).start, block.timestamp);
-        vm.prank(bob);
-        vm.expectRevert("COMBAT SYSTEM: ENTITY OCCUPIED");
-        world.UD__createMatch(EncounterType.PvE, attackers, defenders);
-    }
-
-    function test_EndTurn_EndsMatch() public {
-        StatsData memory startingStats = Stats.get(bobCharacterId);
-        uint256 startingGold = goldToken.balanceOf(bob);
-        vm.prank(bob);
-        bytes32 matchId = world.UD__createMatch(EncounterType.PvE, attackers, defenders);
-        Action[] memory actions = new Action[](1);
-        actions[0] =
-            Action({attackerEntityId: bobCharacterId, defenderEntityId: entityId, actionId: basicAttackId, weaponId: 1});
-        uint256 fees = entropy.getFee(address(1));
-        vm.prank(bob);
-        world.UD__endTurn{value: fees}(matchId, bobCharacterId, actions);
-
-        while (world.UD__getEncounter(matchId).end == 0) {
-            vm.prank(bob);
-            world.UD__endTurn{value: fees}(matchId, bobCharacterId, actions);
-        }
-        StatsData memory endingStats = Stats.get(bobCharacterId);
-        uint256 endingGold = goldToken.balanceOf(bob);
-
-        assertNotEq(startingStats.currentHp, int256(Stats.get(entityId).baseHitPoints));
-        assertGt(endingStats.experience, startingStats.experience);
-        assertGt(endingGold, startingGold);
-    }
-
-    function test_EndTurn_Revert_NonCombatant() public {
-        vm.prank(bob);
-        bytes32 matchId = world.UD__createMatch(EncounterType.PvE, attackers, defenders);
-        Action[] memory actions = new Action[](1);
-        actions[0] =
-            Action({attackerEntityId: bobCharacterId, defenderEntityId: entityId, actionId: basicAttackId, weaponId: 1});
-        uint256 fees = entropy.getFee(address(1));
-        vm.expectRevert("COMBAT SYSTEM: NON-COMBATANT");
-        world.UD__endTurn{value: fees}(matchId, bobCharacterId, actions);
-    }
-
-    function test_CalculateGoldDrop() public {
-        uint256 goldDrop = world.UD__calculateGoldDrop(1, uint256(keccak256("thingy")));
-        assertGt(goldDrop, 0);
-    }
+    function test_physicalAttackWithEquippedWeapon() public {}
 }
