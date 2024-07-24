31ca5606458c8f700fd836a6b00283a0c34ee1d1
MrDeadCe11 <mister.deadce11@gmail.com>
2024-07-23 20:00:09 -0500

    deleted commented code

--- packages/contracts/test/SetUp.sol
+++ packages/contracts/test/SetUp.sol
@@ -70,14 +70,6 @@ contract SetUp is Test {
             hitPointModifier: 4
         });
 
-        // PhysicalAttackStats memory basicAttack = PhysicalAttackStats({
-        //     bonusDamage: 0,
-        //     armorPenetration: 0,
-        //     attackModifierBonus: 0,
-        //     critChanceBonus: 0,
-        //     classRestrictions: classRestrictions
-        // });
-        // basicAttackId = world.UD__createAction(ActionType.PhysicalAttack, abi.encode(basicAttack));
         basicAttackId = keccak256(
             abi.encode(
                 PhysicalAttackStats({
