93048e976f030f110f4cebaf0503c333508217e1
ECWireless <elliott@coopallc.com>
2024-08-04 11:56:54 -0600

    Render all items to StatsPanel

--- packages/client/src/pages/GameBoard.tsx
+++ packages/client/src/pages/GameBoard.tsx
@@ -51,7 +51,7 @@ export const GameBoard = (): JSX.Element => {
     isSynced,
     network: { worldContract },
   } = useMUD();
-  const { character, equippedItems } = useCharacter();
+  const { character, equippedWeapons } = useCharacter();
   const { lastestBattleOutcome, position } = useMapNavigation();
 
   // Redirect to home if synced, but missing other requirements
@@ -77,17 +77,17 @@ export const GameBoard = (): JSX.Element => {
 
   // Open equip info modal if character has no experience and no equipped items
   useEffect(() => {
-    if (!(character && equippedItems)) return;
+    if (!(character && equippedWeapons)) return;
 
     const equipInfoSeenKey = `equip-info-seen-${worldContract.address}-${character.characterId}`;
 
     const hasSeenEquipInfo = localStorage.getItem(equipInfoSeenKey);
     if (hasSeenEquipInfo) return;
 
-    if (character.experience === '0' && equippedItems.length === 0) {
+    if (character.experience === '0' && equippedWeapons.length === 0) {
       onOpenEquipInfoModal();
     }
-  }, [character, equippedItems, onOpenEquipInfoModal, worldContract]);
+  }, [character, equippedWeapons, onOpenEquipInfoModal, worldContract]);
 
   const onAcknowledgeEquipInfo = useCallback(() => {
     if (!character) return;
@@ -214,6 +214,7 @@ export const GameBoard = (): JSX.Element => {
           <Text
             as={Link}
             color="blue"
+            onClick={onAcknowledgeEquipInfo}
             to={`/characters/${character?.characterId}`}
             _hover={{
               textDecoration: 'underline',
