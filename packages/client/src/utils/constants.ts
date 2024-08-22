a30c677272ae884517870ea2cb8b39e4be1889b4
ECWireless <elliott@coopallc.com>
2024-08-22 17:53:09 -0600

    New ABI for AuctionSystem

--- packages/client/src/utils/constants.ts
+++ packages/client/src/utils/constants.ts
@@ -6,6 +6,7 @@ export const MAX_EQUIPPED_WEAPONS = 2;
 export const BATTLE_OUTCOME_SEEN_KEY = 'latest-battle-outcome-seen';
 export const CURRENT_BATTLE_MONSTER_TURN_KEY = 'current-battle-monster-turn';
 export const CURRENT_BATTLE_USER_TURN_KEY = 'current-battle-user-turn';
+
 export const ERC_1155ABI = [
   {
     inputs: [
