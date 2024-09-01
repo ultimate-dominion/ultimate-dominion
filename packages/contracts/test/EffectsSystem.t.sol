1c2d68b76f3b877eadab574ab7d567d5880555ba
MrDeadCe11 <mister.deadce11@gmail.com>
2024-09-01 18:02:18 -0500

    missing files

---
+++ packages/contracts/test/EffectsSystem.t.sol
@@ -0,0 +1,88 @@
+pragma solidity >=0.8.24;
+
+import {SetUp} from "./SetUp.sol";
+import {Classes, ItemType, EncounterType} from "@codegen/common.sol";
+import {StatsData, Stats} from "@tables/Stats.sol";
+import {EncounterEntity} from "@tables/EncounterEntity.sol";
+import "forge-std/console.sol";
+import {PuppetModule} from "@latticexyz/world-modules/src/modules/puppet/PuppetModule.sol";
+import {UltimateDominionConfig} from "@codegen/index.sol";
+import {UltimateDominionConfigSystem} from "@systems/UltimateDominionConfigSystem.sol";
+import {ERC1155Module} from "@erc1155/ERC1155Module.sol";
+import {ERC1155System} from "@erc1155/ERC1155System.sol";
+import {IERC1155MetadataURI} from "@erc1155/IERC1155MetadataURI.sol";
+import {IERC1155} from "@erc1155/IERC1155.sol";
+import {registerERC1155} from "@erc1155/registerERC1155.sol";
+import {_erc1155SystemId} from "@erc1155/utils.sol";
+import {Action, AdjustedCombatStats} from "@interfaces/Structs.sol";
+import {ResourceIdLib} from "@latticexyz/store/src/ResourceId.sol";
+import {ResourceId, WorldResourceIdLib, WorldResourceIdInstance} from "@latticexyz/world/src/WorldResourceId.sol";
+import {
+    CombatEncounterData,
+    MagicDamageStats,
+    PhysicalDamageStatsData,
+    StatusEffectStatsData,
+    StatusEffectValidityData
+} from "@codegen/index.sol";
+import {_mobSystemId} from "../src/utils.sol";
+import {GasReporter} from "@latticexyz/gas-report/src/GasReporter.sol";
+
+contract Test_EffectsSystem is SetUp, GasReporter {
+    bytes32[] public defenders;
+    bytes32[] public attackers;
+    bytes32[] public pvpDefenders;
+    bytes32 entityId;
+    bytes32 entityId2;
+
+    function setUp() public virtual override {
+        super.setUp();
+        vm.prank(deployer);
+        world.UD__setAdmin(address(this), true);
+
+        vm.prank(deployer);
+        world.grantAccess(_mobSystemId("UD"), address(this));
+
+        entityId = world.UD__spawnMob(1, 0, 1);
+        entityId2 = world.UD__spawnMob(1, 0, 1);
+
+        vm.startPrank(alice);
+        world.UD__rollStats(alicesRandomness, alicesCharacterId, Classes.Rogue);
+        world.UD__enterGame(alicesCharacterId);
+        vm.stopPrank();
+
+        // alice has lower agi to go second
+        StatsData memory alicesStats = world.UD__getStats(alicesCharacterId);
+        alicesStats.agility = 9;
+        world.UD__adminSetStats(alicesCharacterId, alicesStats);
+
+        // bob has higher agi to go first
+        StatsData memory BobStats = world.UD__getStats(bobCharacterId);
+        BobStats.agility = 10;
+        world.UD__adminSetStats(bobCharacterId, BobStats);
+
+        // spawn characters
+        vm.prank(bob);
+        world.UD__spawn(bobCharacterId);
+        vm.prank(alice);
+        world.UD__spawn(alicesCharacterId);
+
+        vm.prank(bob);
+        world.UD__move(bobCharacterId, 0, 1);
+        vm.prank(alice);
+        world.UD__move(alicesCharacterId, 0, 1);
+
+        defenders.push(entityId);
+        attackers.push(bobCharacterId);
+        pvpDefenders.push(alicesCharacterId);
+    }
+
+    function test_applyStatusEffect() public {
+        StatsData memory startingStats = world.UD__getStats(bobCharacterId);
+        console.logInt(startingStats.agility);
+        world.UD__adminApplyStatusEffect(
+            bobCharacterId, bytes32(0xd2812fe9b0b2cad2000000000000000000000000000000000000000000000000)
+        );
+        StatsData memory endingStats = world.UD__getStats(bobCharacterId);
+        assertEq(endingStats.agility, startingStats.agility - int256(10));
+    }
+}
