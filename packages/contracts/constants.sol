0cca2ad1904abbe14c149d232633622090b121f7
ECWireless <elliott@coopallc.com>
2024-09-04 16:34:16 -0600

    Get frontend working with smart contract changes

--- packages/contracts/constants.sol
+++ packages/contracts/constants.sol
@@ -11,11 +11,11 @@ string constant ERC721_SYMBOL = "UDC";
 string constant TOKEN_URI = "ipfs://";
 
 uint256 constant DEFAULT_MAX_TURNS = 15;
-uint256 constant TO_HIT_MODIFIER = 1.2 ether;
+uint256 constant TO_HIT_MODIFIER = 1.4 ether;
 uint256 constant DEFENSE_MODIFIER = 1 ether;
 uint256 constant ATTACK_MODIFIER = 1 ether;
 // the amount crits damage is multiplied by
-uint256 constant CRIT_MULTIPLIER = 2;
+uint256 constant CRIT_MULTIPLIER = 4;
 // attack roll has to be CRIT_MODIFIER times greater than the defense roll in order to crit
 uint256 constant CRIT_MODIFIER = 9;
 // the character's stats are divided by STAT_MODIFIER when applying stat bonuses
